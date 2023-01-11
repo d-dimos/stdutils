8eb747caf6bdf107a758b21a328aa8c1f4b8d7c3
Dimitris Dimos <dimitris.dimos647@gmail.com>
2023-01-11 16:48:34 +0200

    changing __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 from __future__ import print_function, absolute_import
 
+from .args import get_args
+from .dirs import create_dirs
+from .logger import configure_logger
 from .print import pretty
 
