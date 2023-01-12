f5e7e392e98b4db4438086758939bc8c13a98d96
Dimitris Dimos <dimitris.dimos647@gmail.com>
2023-01-12 15:04:24 +0200

    minor change

--- print.py
+++ print.py
@@ -2,22 +2,12 @@ from argparse import Namespace
 import logging
 
 
-def pretty(namespace, indent=0, logger=True):
-    if logger:
-        for key, value in vars(namespace).items():
-            output_text = ' ' * indent + str(key) + ': '
-            # logging.info(' ' * indent + str(key))
-            if isinstance(value, Namespace):
-                logging.info(output_text)
-                pretty(value, indent + 2)
-            else:
-                logging.info(' ' * (indent + 2) + output_text + str(value))
-    else:
-        output_text = ""
-        for key, value in vars(namespace).items():
-            output_text += ' ' * indent + str(key) + '\n'
-            if isinstance(value, Namespace):
-                output_text += pretty(value, indent + 2)
-            else:
-                output_text += ' ' * (indent + 2) + str(value) + '\n'
-        return output_text
+def pretty(namespace, indent=0):
+    for key, value in vars(namespace).items():
+        output_text = ' ' * indent + str(key) + ': '
+        # logging.info(' ' * indent + str(key))
+        if isinstance(value, Namespace):
+            logging.info(output_text)
+            pretty(value, indent + 2)
+        else:
+            logging.info(' ' * (indent + 2) + output_text + str(value))
