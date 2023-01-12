33a506b592961e107b2e00a7fdfcf416a9ada881
Dimitris Dimos <dimitris.dimos647@gmail.com>
2023-01-12 15:01:47 +0200

    minor change

--- print.py
+++ print.py
@@ -5,11 +5,13 @@ import logging
 def pretty(namespace, indent=0, logger=True):
     if logger:
         for key, value in vars(namespace).items():
-            logging.info(' ' * indent + str(key))
+            output_text = ' ' * indent + str(key) + ': '
+            # logging.info(' ' * indent + str(key))
             if isinstance(value, Namespace):
+                logging.info(output_text)
                 pretty(value, indent + 2)
             else:
-                logging.info(' ' * (indent + 2) + str(value))
+                logging.info(' ' * (indent + 2) + output_text + str(value))
     else:
         output_text = ""
         for key, value in vars(namespace).items():
