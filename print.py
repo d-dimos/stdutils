79a9263418a92abe8230aefdc0c49be4f74c9d0f
Dimitris Dimos <dimitris.dimos647@gmail.com>
2023-01-12 14:55:21 +0200

    minor change

--- print.py
+++ print.py
@@ -5,9 +5,9 @@ import logging
 def pretty(namespace, indent=0, logger=True):
     if logger:
         for key, value in vars(namespace).items():
-            if key is None:
+            if value is None:
                 continue
-            logging.info(' ' * indent + str(key))
+            logging.info(' ' * indent + str(key)+"{{{")
             if isinstance(value, Namespace):
                 logging.info(pretty(value, indent + 2))
             else:
